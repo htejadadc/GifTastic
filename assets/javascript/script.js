72a3de59d1d984c884bdc789c9c017f16092c899
Hernan Tejada <hernan.tejada@gmail.com>
2017-06-28 00:03:55 -0400

    initial files

---
+++ assets/javascript/script.js
@@ -0,0 +1,49 @@
+var topics = ["Surfing", "Free-Diving", "Spear-Fishing", "Rugby"];
+    
+function displayGiphy() {
+
+  var topic = $(this).attr("entry-name");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+ 
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {        
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var topicImage = $("<img>");
+        topicImage.attr("src", results[i].images.fixed_height.url);
+        gifDiv.prepend(p);
+        gifDiv.prepend(topicImage);
+        $("#giphy-view").prepend(gifDiv);
+      }
+    });
+
+function buttonsDisplay() {
+
+  $("#buttons-display").empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    var a = $("<button>");
+    $("<button>").addClass("topic");
+    $("<button>").attr("entry-name", topics[i]);
+    $("<button>").text(topics[i]);
+    $("#buttons-display").append(a);
+  }
+}
+
+$("#add-topic").on("click", function(event) {
+  event.preventDefault();        
+  var movie = $("#topics-input").val().trim();        
+  topics.push(topic);       
+  buttonsDisplay();
+  $("#topics-input").val('');
+});
+
+$(document).on("click", ".topic", displayGiphy);
+
+buttonsDisplay();
